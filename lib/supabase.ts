b30313e005f766c0adb840cc68f90320d5c81cb4
laughoutloudjune-wq <laughoutloud.june@gmail.com>
2026-02-08 00:27:34 +0700

    Delete lib directory

--- lib/supabase.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-// These lines read the keys you put in .env.local
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-
-// This creates the "connection" that we can use in other files
-export const supabase = createClient(supabaseUrl, supabaseKey)
